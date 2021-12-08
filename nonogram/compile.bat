84a3a41e13aa42cc819764d41201d66eb31d84ca
CompileError <gngn15401@gmail.com>
2021-12-09 02:50:06 +0800

    Done

--- nonogram/compile.bat
+++ nonogram/compile.bat
@@ -1,1 +1,1 @@
-g++ -o nonogram nonogram.cpp Board.cpp Unit.cpp FP1.cpp Collector.cpp Propogate.cpp Set.cpp -O3
+g++ -o nonogram nonogram.cpp Board.cpp Unit.cpp Backtrack.cpp FP1.cpp Collector.cpp Propogate.cpp Set.cpp -O3
